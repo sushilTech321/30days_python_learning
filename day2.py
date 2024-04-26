0706a1ff1320d64b8383c7430b387f540b6fef6d
sushil <monster321421@gmail.com>
2024-04-27 00:29:19 +0545

    added 'extend()' method @14  and @24

--- day2.py
+++ day2.py
@@ -1,7 +1,5 @@
 # Python - Add List Items
 # Append Items -> append() method, To add an item to the end of the list
-
-
 thislist = ["apple", "banana", "cherry"]
 thislist.append("orange")
 print(thislist)
@@ -11,4 +9,18 @@ print(thislist)
 thislist1 = ["car", "bus", "van","motorbike"]
 thislist1.insert(2, "bycycle")
 print(thislist1)
+
+
+# Extend List -> extend() method, append elements from another list to the current list
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+
+# Add Any Iterable, Add elements of a tuple to a list:
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
 
