8172e702ae07e918bec1b2c6febc53e5353128dd
sushil <monster321421@gmail.com>
2024-04-27 00:07:23 +0545

    Added 'bycycle' to the list using insert() method #13

--- day2.py
+++ day2.py
@@ -6,3 +6,9 @@ thislist = ["apple", "banana", "cherry"]
 thislist.append("orange")
 print(thislist)
 
+
+# Insert Items -> insert() method, insert a list item at a specified index,
+thislist1 = ["car", "bus", "van","motorbike"]
+thislist1.insert(2, "bycycle")
+print(thislist1)
+
