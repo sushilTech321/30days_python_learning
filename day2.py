44d438807d394db7198192319def2c235067e3d0
sushil <monster321421@gmail.com>
2024-04-26 23:57:48 +0545

    Add 'orange' to the list

---
+++ day2.py
@@ -0,0 +1,8 @@
+# Python - Add List Items
+# Append Items -> append() method, To add an item to the end of the list
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
