031d488962b4213d627110665f65755b931d3b77
sushil <monster321421@gmail.com>
2024-04-27 00:43:43 +0545

    Refactor list manipulation operations

---
+++ day3.py
@@ -0,0 +1,12 @@
+# Day 3 , I going to learng, Python - Remove List Items
+# remove() -> method removes the specified item
+# pop() -> method removes the specified index, if do not specify the index removes the last item.
+# del -> keyword also removes the specified index , also delete the list completely.    
+# clear() ->  method empties the list , list still remains, but it has no conten
+
+thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# thislist.pop(1)
+# del thislist[0]
+thislist.clear()
+print(thislist)
