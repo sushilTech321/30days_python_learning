3a3a8b3f580ac9c905d9b8ac89476cb40f80c90e
sushil <monster321421@gmail.com>
2024-05-02 00:58:42 +0545

    modified

--- day6.py
+++ day6.py
@@ -51,7 +51,7 @@ set2 = {"aeroplane","bus","apple"}
 # set3 = set1.union(set2)
 # set1.update(set2)
 # set3 = set1.intersection(set2)
-set1.intersection_update(set2)
+# set1.intersection_update(set2)
 # set3 = set1.difference(set2)
 # set3 = set1.symmetric_difference(set2)
 set1.symmetric_difference_update(set2)
