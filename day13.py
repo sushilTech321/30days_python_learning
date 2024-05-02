8c1b8b2416f7bc45a839b5e32e7a6e352076f196
sushil <monster321421@gmail.com>
2024-05-02 23:50:22 +0545

    learned about polymorphism

---
+++ day13.py
@@ -0,0 +1,45 @@
+# class polymorphism 
+
+class Car :
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("move")
+        
+class Boat: 
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Sail")
+
+class Plane: 
+    def __init__(self, model, brand):
+        self.model = model
+        self.brand = brand
+
+    def move(self):
+        print("fly")
+
+c = Car("Ford","Mustang")
+b = Boat("Local","dunga")
+p = Plane("Hawai","Jahaj")
+
+# c.move()
+# b.move()
+# p.move()
+
+for x in (c,b,p):
+    # print(x.model)
+    # print(x.brand)
+    # print(x)
+    x.move()
+
+def myfunc():
+    print("hello")
+myfunc()
+
+
