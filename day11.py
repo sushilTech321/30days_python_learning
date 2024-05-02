cc57a520259a7ac460cd7894af30d25115e8c12e
sushil <monster321421@gmail.com>
2024-05-02 23:47:39 +0545

    bug fixed

--- day11.py
+++ day11.py
@@ -1,42 +1,17 @@
-# Python code to demonstrate how parent constructors
-# are called.
+# python -> inheritance 
 
-# parent class
-class Person:
-
-    # __init__ is known as the constructor
-    def __init__(self, name, idnumber):
+class Person :
+    def __init__(self, name, rollno, age):
         self.name = name
-        self.idnumber = idnumber
+        self.rollno = rollno
+        self.age = age
 
-    def display(self):
-        print(self.name)
-        print(self.idnumber)
-        
-    def details(self):
-        print("My name is {}".format(self.name))
-        print("IdNumber: {}".format(self.idnumber))
-    
-# child class
-class Employee(Person):
-    def __init__(self, name, idnumber, salary, post):
-        self.salary = salary
-        self.post = post
+    def printname(self):
+        print(self.name, self.rollno, self.age)
 
-        # invoking the __init__ of the parent class
-        Person.__init__(self, name, idnumber)
-        
-    def details(self):
-        print("My name is {}".format(self.name))
-        print("IdNumber: {}".format(self.idnumber))
-        print("Post: {}".format(self.post))
-        print("salary: {}".format(self.salary))
+class Student(Person):
+    def __init__(self, name, rollno, age):
+        super().__init__(self, name, rollno, age)   
 
-
-# creation of an object variable or an instance
-a = Employee('Rahul', 886012, 200000, "Intern")
-
-# calling a function of the class Person using
-# its instance
-a.display()
-a.details()
+s = Person("mike",1,20)
+s.printname()        
